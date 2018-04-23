31945a1504e60d948b3af401a12932e5340affbc
Michael R Bynum <michael.r.bynum@intel.com>
2018-04-22 21:03:25 -0700

    Converted all strings to quotation marks; organized init into subfunctions

--- slideshow.py
+++ slideshow.py
@@ -10,28 +10,18 @@ class HelloWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
 
+        self.initWindow()
+        self.initMenuBar()
+        self.initBody()
+
+        # Inform user that we"re ready for use
+        self.statusBar().showMessage("Ready!")
+
+    def initWindow(self):
         self.setMinimumSize(QSize(640, 480))
-        self.setWindowTitle('Hello World')
+        self.setWindowTitle("Hello World")
 
-        # Initialize menu bar
-        menubar = self.menuBar()
-        menubar.setNativeMenuBar(False)
-
-        # Initialize File menu
-
-        openAction = QAction('Open', self)
-        openAction.triggered.connect(self.openFile)
-        openAction.setStatusTip('Open File')
-
-        quitAction = QAction('Quit', self)
-        quitAction.triggered.connect(self.close)
-        quitAction.setStatusTip('Quit Application')
-        quitAction.setShortcut('Ctrl+Q')
-
-        fileMenu = menubar.addMenu('File')
-        fileMenu.addAction(openAction)
-        fileMenu.addAction(quitAction)
-
+    def initBody(self):
         # Setup central grid layout
         centralWidget = QWidget(self)
         self.setCentralWidget(centralWidget)
@@ -40,18 +30,37 @@ class HelloWindow(QMainWindow):
         centralWidget.setLayout(gridLayout)
 
         # Add labels to layout
-        titleLabel = QLabel('Hello World from PyQt', self)
+        titleLabel = QLabel("Hello World from PyQt", self)
         titleLabel.setAlignment(QtCore.Qt.AlignCenter)
 
         self.mainPictureLabel = QLabel(self)
         self.mainPictureLabel.scaleFactor = 1.0
+        self.mainPictureLabel.setAlignment(QtCore.Qt.AlignCenter)
 
         gridLayout.addWidget(titleLabel, 0, 0)
         gridLayout.addWidget(self.mainPictureLabel, 1, 0)
 
-        # Inform user that we're ready for use
-        self.resize(640, 480)
-        self.statusBar().showMessage("Ready!")
+    def initMenuBar(self):
+        # Initialize menu bar
+        menubar = self.menuBar()
+        menubar.setNativeMenuBar(False)
+
+        # Initialize File menu
+
+        openAction = QAction("Open", self)
+        openAction.triggered.connect(self.openFile)
+        openAction.setStatusTip("Open File")
+
+        quitAction = QAction("Quit", self)
+        quitAction.triggered.connect(self.close)
+        quitAction.setStatusTip("Quit Application")
+        quitAction.setShortcut("Ctrl+Q")
+
+        # Build file menu
+        fileMenu = menubar.addMenu("File")
+        fileMenu.addAction(openAction)
+        fileMenu.addSeparator()
+        fileMenu.addAction(quitAction)
 
     def openFile(self):
         self.imageFilename, _ = QFileDialog.getOpenFileName(
@@ -61,7 +70,7 @@ class HelloWindow(QMainWindow):
         self.mainPictureLabel.setPixmap(QPixmap.fromImage(image))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     mainWin = HelloWindow()
     mainWin.show()
