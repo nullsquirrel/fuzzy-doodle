7da96588496ece8b370361c320f6cb2b4c279d91
Michael R Bynum <michael.r.bynum@intel.com>
2018-05-07 12:23:46 -0700

    Updated min size to 320x240; Added buttons; text changes with image; renamed objects away from beginner 'Hello World'

--- slideshow.py
+++ slideshow.py
@@ -1,25 +1,32 @@
 import sys
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtWidgets import (QMainWindow, QFileDialog, QLabel, QGridLayout,
-                             QWidget, QAction)
+                             QWidget, QAction, QPushButton, QHBoxLayout,
+                             QSizePolicy)
 from PyQt5.QtCore import QDir, QSize
 from PyQt5.QtGui import QPixmap, QImage
 
 
-class HelloWindow(QMainWindow):
+class SlideshowWindow(QMainWindow):
+
     def __init__(self):
         QMainWindow.__init__(self)
 
+        # Capture screen geometry prior to GUI initialization
+        self.screenGeometry = QtWidgets.QApplication.desktop().screenGeometry()
+
+        # Initialize GUI
         self.initWindow()
         self.initMenuBar()
         self.initBody()
 
-        # Inform user that we"re ready for use
-        self.statusBar().showMessage("Ready!")
+        # Inform user that we're ready for use
+        self.statusBar().showMessage('Ready!')
 
     def initWindow(self):
-        self.setMinimumSize(QSize(640, 480))
-        self.setWindowTitle("Hello World")
+        self.setMinimumSize(QSize(320, 240))
+        self.setWindowTitle('Fuzzy Doodle Sketch Practice')
+
 
     def initBody(self):
         # Setup central grid layout
@@ -29,49 +36,92 @@ class HelloWindow(QMainWindow):
         gridLayout = QGridLayout(self)
         centralWidget.setLayout(gridLayout)
 
-        # Add labels to layout
-        titleLabel = QLabel("Hello World from PyQt", self)
-        titleLabel.setAlignment(QtCore.Qt.AlignCenter)
+        self.initPictureFrame()
+        self.initBodyButtons()
 
+        # Add widgets to layout
+        gridLayout.addWidget(self.mainPictureLabel, 0, 0)
+        gridLayout.addWidget(self.titleLabel, 1, 0)
+        gridLayout.addLayout(self.buttonLayout, 2, 0)
+
+
+    def initPictureFrame(self):
+        # Create Title Label
+        self.titleLabel = QLabel('Fuzzy Doodle Sketch Practice', self)
+        self.titleLabel.setAlignment(QtCore.Qt.AlignCenter)
+        self.titleLabel.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
+
+        # Create main image label
         self.mainPictureLabel = QLabel(self)
         self.mainPictureLabel.scaleFactor = 1.0
         self.mainPictureLabel.setAlignment(QtCore.Qt.AlignCenter)
+        self.mainPictureLabel.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
+        self.mainPictureLabel.setScaledContents(True)
 
-        gridLayout.addWidget(titleLabel, 0, 0)
-        gridLayout.addWidget(self.mainPictureLabel, 1, 0)
+
+    def initBodyButtons(self):
+        # Create back button
+        self.backButton = QPushButton('Back', self)
+        self.backButton.setToolTip('Go to previous image')
+        self.backButton.clicked.connect(self.nextButtonAction)
+        self.backButton.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
+
+        # Create pause/play button
+        self.playPauseButton = QPushButton('Play', self)
+        self.playPauseButton.setToolTip('Start/continue practice session')
+        self.playPauseButton.clicked.connect(self.nextButtonAction)
+        self.playPauseButton.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
+
+        # Create next button
+        self.nextButton = QPushButton('Next', self)
+        self.nextButton.setToolTip('Go to next image')
+        self.nextButton.clicked.connect(self.nextButtonAction)
+        self.nextButton.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
+
+        # Create button widget
+        self.buttonLayout = QHBoxLayout(self)
+        self.buttonLayout.addWidget(self.backButton)
+        self.buttonLayout.addWidget(self.playPauseButton)
+        self.buttonLayout.addWidget(self.nextButton)
+
 
     def initMenuBar(self):
         # Initialize menu bar
         menubar = self.menuBar()
         menubar.setNativeMenuBar(False)
 
-        # Initialize File menu
-
-        openAction = QAction("Open", self)
+        # Initialize File menu components
+        openAction = QAction('Open', self)
         openAction.triggered.connect(self.openFile)
-        openAction.setStatusTip("Open File")
+        openAction.setStatusTip('Open File')
 
-        quitAction = QAction("Quit", self)
+        quitAction = QAction('Quit', self)
         quitAction.triggered.connect(self.close)
-        quitAction.setStatusTip("Quit Application")
-        quitAction.setShortcut("Ctrl+Q")
+        quitAction.setStatusTip('Quit Application')
+        quitAction.setShortcut('Ctrl+Q')
 
         # Build file menu
-        fileMenu = menubar.addMenu("File")
+        fileMenu = menubar.addMenu('File')
         fileMenu.addAction(openAction)
         fileMenu.addSeparator()
         fileMenu.addAction(quitAction)
 
+
     def openFile(self):
         self.imageFilename, _ = QFileDialog.getOpenFileName(
-            self, "Open File", QDir.currentPath())
+            self, 'Open File', QDir.currentPath())
 
         image = QImage(self.imageFilename)
         self.mainPictureLabel.setPixmap(QPixmap.fromImage(image))
+        self.titleLabel.setText(self.imageFilename)
 
 
-if __name__ == "__main__":
+    def nextButtonAction(self):
+        self.mainPictureLabel.clear()
+
+
+if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    mainWin = HelloWindow()
+    mainWin = SlideshowWindow()
     mainWin.show()
     sys.exit(app.exec_())
