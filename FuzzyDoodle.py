75a78c1372e48767705d6d01ceee735fc831b4a4
Michael R Bynum <michael.r.bynum@intel.com>
2019-03-02 08:35:20 -0800

    Fixed error calling module rather than class.

--- FuzzyDoodle.py
+++ FuzzyDoodle.py
@@ -3,6 +3,6 @@ from PyQt5 import QtWidgets
 import FuzzyDoodlePlayer
 
 app = QtWidgets.QApplication(sys.argv)
-mainWin = FuzzyDoodlePlayer()
+mainWin = FuzzyDoodlePlayer.FuzzyDoodlePlayer()
 mainWin.ui.show()
 sys.exit(app.exec_())
